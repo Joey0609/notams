87f46a22992004f71f49b51231a6f725b9e53c33
FallingFengre <liujunye_tom@126.com>
2024-12-01 02:22:16 +0800

    v1.3

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def fetch():
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
     }
     data = {
-        "retrieveLocId": "  ", 
+        "retrieveLocId": "ZBPE ZGZU ZHWH ZJSA ZLHW ZPKM ZSHA ZWUQ ZYSH RPHI VVHM WSJC", 
         "reportType": "Report", 
         "actionType": "notamRetrievalByICAOs",
     }
