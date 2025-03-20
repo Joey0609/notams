346c7a05c72967cca8d8dd7dd80b7b84dc780ccc
FallingFengre <liujunye_tom@126.com>
2025-03-20 13:21:39 +0800

    v1.5

--- scripts/notamsDrawer.js
+++ scripts/notamsDrawer.js
@@ -23,8 +23,13 @@ function toggleDrawer() {
 function drawWarning() {
     const text = document.getElementById('inputText').value.trim();
     const color = document.getElementById('colorSelect').value;
-    const coordPattern = /N\d{4,6}E\d{5,7}/;
-    if (!coordPattern.test(text)) {
+    const coordPattern = /[NS]\d{4,6}[WE]\d{5,7}/;
+    const coordPattern2= /\d{4,6}[NS]\d{5,7}[WE]/;
+    let targ=text.replace(/{[^}]+}/g, '');
+    targ = targ.replace(/%[^%]+%/g, ''); 
+    targ = targ.replace(/[\r\n]+/g, '');
+    targ = targ.replace(/\s+/g, '');
+    if (!coordPattern.test(targ)&&!coordPattern2.test(targ)) {
         alert("请输入正确的航警，格式应包含有效的坐标\n（如：N394838E1005637-N391617E1005933-N392001E1021555-N395223E1021334）");
         return;
     }
@@ -66,15 +71,48 @@ function removeObj() {
         warningList.removeChild(warningList.firstChild);
     }
 }
-
+function processCoordinates(textCforCoor) {
+    const subPattern = /\d{6}[NS]\d{7}[WE]/g;
+    const tmp = textCforCoor.match(subPattern) || [];
+    if (tmp.length > 2) {
+        const result = tmp.map(m => 
+            m[6] + m.substring(0, 6) + m.slice(-1) + m.substring(7, m.length - 1)
+        ).join('-');
+        let st=[];
+        st[0]=result;
+        return st;
+    }
+    return null;
+}
+function processCoordinates2(textCforCoor) {
+    const subPattern = /\d{4}[NS]\d{5}[WE]/g;
+    const tmp = textCforCoor.match(subPattern) || [];
+    if (tmp.length > 2) {
+        const result = tmp.map(m => 
+            m[4] + m.substring(0, 4) + m.slice(-1) + m.substring(5, m.length - 1)
+        ).join('-');
+        let st=[];
+        st[0]=result;
+        return st;
+    }
+    return null;
+}
 function selfDrawNot(text, color, num) {
     text = text.replace(/{[^}]+}/g, ''); 
     text = text.replace(/%[^%]+%/g, ''); 
     text = text.replace(/[\r\n]+/g, '');
-    const coorRegex = /N\d{4,6}E\d{5,7}(?:-N\d{4,6}E\d{5,7})*/g;
+    text = text.replace(/\s+/g, '');
+    const coorRegex = /[NS]\d{4,6}[WE]\d{5,7}(?:-[NS]\d{4,6}[WE]\d{5,7})*/g;
     // const timeRegex = /(\d{2} [A-Z]{3} \d{2}:\d{2} \d{4} UNTIL \d{2} [A-Z]{3} \d{2}:\d{2} \d{4})/;
     const codeRegex = /A\d{4}\/\d{2}/g;
-    const coor = text.match(coorRegex);
+    let coor = text.match(coorRegex);
+    if(coor==null){
+        coor=processCoordinates(text);      
+    }
+    if(coor==null){
+        coor=processCoordinates2(text)
+    }
+    // alert(coor);
     // const time = text.match(timeRegex);
     const code = text.match(codeRegex);
     // alert(coor[0]);
